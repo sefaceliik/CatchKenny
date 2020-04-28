6f6e1d05ac792b4f4a0d9c6b4e06cec5fa5846ed
sefacelik <sefacelik1800@gmail.com>
2020-04-29 01:56:31 +0300

    Initial Commit

---
+++ CatchTheKenny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheKenny
+//
+//  Created by Sefa Çelik on 29.04.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
