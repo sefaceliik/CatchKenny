c9711f6a4c90dbc0be4aabbd13df502704d46633
sefacelik <sefacelik1800@gmail.com>
2020-06-17 01:40:58 +0300

    no message

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -9,12 +9,137 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var score = 0
+    var timer = Timer()
+    var counter = 0
+    var imageArray = [UIImageView]()
+    var hideTimer = Timer()
+    var highscore = 0
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
+    @IBOutlet weak var image5: UIImageView!
+    @IBOutlet weak var image6: UIImageView!
+    @IBOutlet weak var image7: UIImageView!
+    @IBOutlet weak var image8: UIImageView!
+    @IBOutlet weak var image9: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        let storedHighscore = UserDefaults.standard.object(forKey: "highscore")
+        
+        if storedHighscore == nil {
+            highscore = 0
+            highscoreLabel.text = "Highscore: \(highscore)"
+        }
+        if let newScore = storedHighscore as? Int {
+            highscore = newScore
+            highscoreLabel.text = "Highscore: \(highscore)"
+        }
+        
+        image1.isUserInteractionEnabled = true
+        image2.isUserInteractionEnabled = true
+        image3.isUserInteractionEnabled = true
+        image4.isUserInteractionEnabled = true
+        image5.isUserInteractionEnabled = true
+        image6.isUserInteractionEnabled = true
+        image7.isUserInteractionEnabled = true
+        image8.isUserInteractionEnabled = true
+        image9.isUserInteractionEnabled = true
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+    
+        image1.addGestureRecognizer(recognizer1)
+        image2.addGestureRecognizer(recognizer2)
+        image3.addGestureRecognizer(recognizer3)
+        image4.addGestureRecognizer(recognizer4)
+        image5.addGestureRecognizer(recognizer5)
+        image6.addGestureRecognizer(recognizer6)
+        image7.addGestureRecognizer(recognizer7)
+        image8.addGestureRecognizer(recognizer8)
+        image9.addGestureRecognizer(recognizer9)
+        
+        imageArray = [image1,image2,image3,image4,image5,image6,image7,image8,image9]
+        
+        counter = 10
+        timerLabel.text = "\(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideImage), userInfo: nil, repeats: true)
+        hideImage()
+        
     }
+    
+    @objc func hideImage() {
+        
+        for image in imageArray {
+            image.isHidden = true
+        }
+        let random = Int(arc4random_uniform(UInt32(imageArray.count - 1)))
+        imageArray[random].isHidden = false
+    }
+    
+    @objc func countDown() {
+        counter -= 1
+        timerLabel.text = String(counter)
+        
+        if counter == 0 {
+            timer.invalidate()
+            hideTimer.invalidate()
 
+            for image in imageArray {
+                image.isHidden = true
+            }
+            
+            if self.score > self.highscore {
+                self.highscore = self.score
+                highscoreLabel.text = "Highscore: \(self.highscore)"
+                UserDefaults.standard.set(self.highscore, forKey: "highscore")
+            }
+            
+            let alert = UIAlertController(title: "Time is Up!", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let replayButton = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default) { (UIAlertAction) in
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 10
+                self.timerLabel.text = String(self.counter)
+                
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideImage), userInfo: nil, repeats: true)
+            }
+            
+            alert.addAction(okButton)
+            alert.addAction(replayButton)
+            self.present(alert,animated: true,completion: nil)
+        }
+        
+    }
+    
+    @objc func increaseScore() {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+    }
 
 }
 
